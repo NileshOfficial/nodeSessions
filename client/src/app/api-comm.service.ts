e38a120886d82eaff30561483462a231e70e2a7f
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-28 21:31:22 +0530

    api service

---
+++ client/src/app/api-comm.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiCommService {
+
+  constructor(private http: HttpClient) { }
+
+  getUserData(): Observable<Array<any>> {
+    return this.http.get<Array<any>>("http://localhost:3001/users");
+  }
+
+  getUserAutoCompleteList(query: string): Observable<Array<string>> {
+    return this.http.get<Array<string>>("http://localhost:3001/users", {
+      params: new HttpParams().set('search', query)
+    });
+  }
+
+
+  getStudentData(): Observable<Array<any>> {
+    return this.http.get<Array<any>>("http://localhost:3001/students");
+  }
+
+  filterStudentByBranch(query: string): Observable<Array<any>> {
+    return this.http.get<Array<any>>("http://localhost:3001/students", {
+      params: new HttpParams().set('search', query)
+    });
+  }
+}
