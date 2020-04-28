0f38381846463597c4b29e5731cab70dfdab904e
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-28 22:25:45 +0530

    client complete

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Q1Q2Component } from './q1-q2/q1-q2.component';
+import { Q3Component } from './q3/q3.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "Q1-Q2", pathMatch: "full"},
+  {path:"Q1-Q2", component: Q1Q2Component},
+  {path: "Q3", component: Q3Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
