e9a289dcb39ab933d285e37fa262087fdc41e538
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-30 16:53:26 +0530

    completed gitDetails route

--- routes.js
+++ routes.js
@@ -1,6 +1,15 @@
-const router = require('express').Router();
-exports.router = function() {
-    router.get('/login', (req, res) => {});
-    router.get('/AuthenticRoute', (req, res) => {});
-    router.get('/gitProfile', (req, res)=>{console.log(req.query)});
+const axios = require('axios');
+
+exports.router = function (server) {
+    server.get('/login', (req, res) => { });
+    server.get('/AuthenticRoute', (req, res) => { });
+    server.get('/gitProfile', async (req, res) => {
+        axios.get(`https://api.github.com/users/${req.query.username}`)
+            .then(gitRes => {
+                res.json(gitRes.data);
+            })
+            .catch(() => {
+                res.json({error: true});
+            });
+    });
 }
--- serve.js
+++ serve.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
-
 const port = 3000;
+
+const routes = require('./routes').router;
+
+routes(server);
 
 server.listen(port, () => console.log(`server listening on port ${3000}`));
