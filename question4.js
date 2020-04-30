068ea37a9ee34eda7273e3c64310daeca746466c
NileshOfficial <nilesh.kumar@tothenew.com>
2020-04-30 21:39:32 +0530

    question4 done

--- question4.js
+++ question4.js
@@ -1,8 +1,8 @@
 const { execFile } = require('child_process');
 
-const child = execFile('stat', ['./file.txt'], (error, stdout, stderr) => {
+execFile('stat', ['./file.txt'], (error, stdout, stderr) => {
     if (error) {
-      throw error;
+        throw error;
     }
     console.log(stdout);
-  });
+});
