34413e41e9162de509a420b0a0643fad4a704981
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-05 23:35:32 +0530

    added try catch

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -1,17 +1,37 @@
 const itemDbService = require('../services/item.service');
 
 module.exports.getItems = async (req, res) => {
-    res.json(await itemDbService.getItems());
+    try {
+        res.json(await itemDbService.getItems());
+    } catch(err) {
+        res.status(500);
+        res.send("server error");
+    }
 }
 
 module.exports.addItem = async (req, res) => {
-    res.json(await itemDbService.addItem(req.body));
+    try {
+        res.json(await itemDbService.addItem(req.body));
+    } catch(err) {
+        res.status(500);
+        res.send("server error");
+    }
 }
 
 module.exports.updateExistingItem = async (req, res) => {
-    res.json(await itemDbService.updateExistingItem(req.params.id, req.body));
+    try {
+        res.json(await itemDbService.updateExistingItem(req.params.id, req.body));
+    } catch(err) {
+        res.status(500);
+        res.send("server error");
+    }
 }
 
 module.exports.deleteItem = async (req, res) => {
-    res.json(await itemDbService.deleteItem(req.params.id));
+    try {
+        res.json(await itemDbService.deleteItem(req.params.id));
+    } catch(err) {
+        res.status(500);
+        res.send("server error");
+    }
 }
