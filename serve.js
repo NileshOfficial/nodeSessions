29d51576977ecfce1eb754ee8f282cbe487457a1
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-05 21:27:33 +0530

    added option to mongoose.connect

--- serve.js
+++ serve.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const goose = require('mongoose');
+
 const routes = require("./routes");
+
 const server = express();
 const PORT = 3000;
 
 server.use(express.json());
 server.use("/", routes);
 
-server.listen(PORT, () => { 
-    console.log(`server running on port: ${PORT}`) 
+goose.connect("mongodb://localhost:2000/GroceriesInventory", {
+    useNewUrlParser: "true",
+    useFindAndModify: false
+});
+
+goose.connection.on("error", err => {
+    console.log("err", err)
+});
+
+goose.connection.on("connected", (err, res) => {
+    console.log("mongoose is connected");
+    
+    server.listen(PORT, () => {
+        console.log(`server running on port: ${PORT}`)
+    });
 });
