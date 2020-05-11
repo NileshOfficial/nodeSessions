30d2660756879e74d55c68db0a870df35f42ac96
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-11 21:43:22 +0530

    added verify method to the tokenService

--- tokenService.js
+++ tokenService.js
@@ -2,4 +2,11 @@ const jwt = require('jsonwebtoken');
 const tokenKey = "NOT_SO_SECRET_KEY";
 
 module.exports.generateToken = (payload) => jwt.sign(payload, tokenKey, { expiresIn: "30m" });
-module.exports.verifyToken = () => {};
+module.exports.verifyToken = (token) => {
+    try {
+        const payload = jwt.verify(token, tokenKey);
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
