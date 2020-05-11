88199e23236716eee25c233b3475ac372f657740
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-11 21:22:16 +0530

    added try catch block

--- service.js
+++ service.js
@@ -4,46 +4,54 @@ const tokenService = require('./tokenService');
 
 module.exports.addUser = async (userDetails) => {
 
-    const existingUser = await userModel.findOne({ email: userDetails.email });
+    try {
+        const existingUser = await userModel.findOne({ email: userDetails.email });
 
-    if (existingUser)
-        return { err: "email already taken" };
-    else {
-        const hashedPassword = await hash.createHash(userDetails.password);
-        userDetails['password'] = hashedPassword;
-        
-        try {
-            await userModel.insertMany([userDetails]);
-        } catch(err) {
-            return { err: "db operation failed" };
-        }
-        
-        const payload = {
-            name: userDetails.name,
-            email: userDetails.email
-        }
-        const token = tokenService.generateToken(payload);
-        return { success: true, token };
-    }
-}
+        if (existingUser)
+            return { err: "email already taken" };
+        else {
+            const hashedPassword = await hash.createHash(userDetails.password);
+            userDetails['password'] = hashedPassword;
 
-module.exports.authenticateLogin = async (credentials) => {
-    const userData = await userModel.findOne({ email: credentials.email });
+            try {
+                await userModel.insertMany([userDetails]);
+            } catch (err) {
+                return { err: "db operation failed" };
+            }
 
-    if (!userData)
-        return { err: "invalid username or password" };
-    else {
-        const match = await hash.compareHash(credentials.password, userData.password);
-
-        if (match) {
             const payload = {
-                name: credentials.name,
-                email: credentials.email
+                name: userDetails.name,
+                email: userDetails.email
             }
             const token = tokenService.generateToken(payload);
             return { success: true, token };
         }
-        else
-            return { err: "invalid username or password" }
+    } catch (error) {
+        return { err: "db operation failed" };
+    }
+}
+
+module.exports.authenticateLogin = async (credentials) => {
+    try {
+        const userData = await userModel.findOne({ email: credentials.email });
+
+        if (!userData)
+            return { err: "invalid username or password" };
+        else {
+            const match = await hash.compareHash(credentials.password, userData.password);
+
+            if (match) {
+                const payload = {
+                    name: credentials.name,
+                    email: credentials.email
+                }
+                const token = tokenService.generateToken(payload);
+                return { success: true, token };
+            }
+            else
+                return { err: "invalid username or password" }
+        }
+    } catch (error) {
+        return { err: "db operation failed" };
     }
 }
