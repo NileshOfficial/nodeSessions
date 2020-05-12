e4688447361aee47aaa561745d4220b8b010711f
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-12 18:45:20 +0530

    oauth setup done

--- passport.config.js
+++ passport.config.js
@@ -1,7 +1,23 @@
 const localStrategy = require('passport-local').Strategy;
+const googleStrategy = require('passport-google-oauth2').Strategy;
 
 const service = require("./service");
 
 module.exports.config = (passport) => {
     passport.use(new localStrategy(service.authenticateLogin));
+    passport.use(new googleStrategy({
+        clientID: "431128297014-66fqj0488jptf5n24ce0vvoo89c3166l.apps.googleusercontent.com",
+        clientSecret: "LZXzLMO6o32WprPIGcAZVF9J",
+        callbackURL: "http://localhost:3000/auth/google/callback",
+        passReqToCallback: true
+    }, function (request, accessToken, refreshToken, profile, done) {
+        return done(null, request);
+    }));
+    passport.serializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function (user, done) {
+        done(null, user);
+    });
 }
--- routes.js
+++ routes.js
@@ -1,9 +1,28 @@
 const router = require('express').Router();
 const controller = require('./controller');
 const midwares = require('./midwares');
+const passport = require('passport');
 
 router.get("/home", midwares.verifyRequest, controller.home);
 router.post("/signup", controller.signup);
 router.post("/login", controller.login);
+router.get('/auth/google',
+  passport.authenticate('google', { scope: 
+      [ 'https://www.googleapis.com/auth/plus.login'] }
+));
+
+router.get( '/auth/google/callback', 
+    passport.authenticate( 'google', { 
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
+
+router.get('/auth/google/success', (req, res) => {
+    res.send("done");
+});
+
+router.get('/auth/google/success', (req, res) => {
+    res.send("not done");
+});
 
 module.exports = router;
